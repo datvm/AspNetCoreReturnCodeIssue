084ba471fb4c1cd36bd723fd592ac72ee9356703
Luke Vo <datvm@outlook.com>
2020-07-08 06:48:51 +0700

    Add project files.

---
+++ WebApplication2/Controllers/TestController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/test")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+
+        [HttpGet, Route("")]
+        public IActionResult GetSomething()
+        {
+            throw new IOException();
+        }
+
+        [HttpPost, Route("")]
+        public IActionResult PostSomething()
+        {
+            throw new IOException();
+        }
+
+        [HttpPut, Route("")]
+        public IActionResult PutSomething()
+        {
+            throw new IOException();
+        }
+
+        [HttpDelete, Route("")]
+        public IActionResult DeleteSomething()
+        {
+            throw new IOException();
+        }
+
+    }
+}
